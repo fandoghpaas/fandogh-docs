ed935d6f440caab6077fe1dd00888a31cc31d6e2
Reza Haidari <shoosh2007@gmail.com>
2018-08-12 15:15:29 +0430

    add gitignore

--- node_modules/emotion-theming/src/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { default as ThemeProvider } from './theme-provider'
-export { default as withTheme } from './with-theme'
-export { channel, contextTypes, createBroadcast } from './utils'
--- node_modules/react-docgen-typescript-loader/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var loader_1 = require("./loader");
-exports.default = loader_1.default;
