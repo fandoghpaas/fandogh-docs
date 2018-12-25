8d29828540bc0248941cd3f70ddf53d269ae7e37
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2018-12-25 11:19:38 +0330

    mattermost added as latest blog post

--- website/articles.js
+++ website/articles.js
@@ -1,5 +1,12 @@
 const articles = [
 	{
+		title: 'استفاده از برنامه Mattermost به عنوان یک سرویس',
+		description: 'برنامه Mattermost یک سرویس پیام‌رسان است که عمده استفاده از آن برای شرکت‌ها بوده و از محبوبیت بالایی برخوردار است. این برنامه این قابلیت را دارد که به صورت رایگان بر روی سرورهای شخصی مورد استفاده قرار گیرد٬' ,
+		image: 'https://blog.fandogh.cloud/articles/mattermost_thumbnail.png',
+		url: 'https://blog.fandogh.cloud/articles/mattermost-deployment-post.html',
+		date:"۱ دی ۱۳۹۷"
+
+	},{
 		title: 'Zero Downtime Deployment',
 		description: 'در این پست به تشریح فرایند استقرار نسخه‌های جدید یک سرویس بر روی فندق می‌پردازیم. این مطالب به شما کمک خواهد کرد تا با مفهوم Zero Downtime Deployment بیشتر آشنا شوید. همچنین برای اینکه فندق بتواند این نوع استقرار را به درستی انجام دهد تنظیماتی از جانب کاربران مورد نیاز است که در این پست به تشریح آنها نیز خواهیم پرداخت.\n' ,
 		image: 'https://blog.fandogh.cloud/articles/zero-downtime.png',
@@ -12,14 +19,6 @@ const articles = [
 		image: 'https://blog.fandogh.cloud/articles/shared_storage.png',
 		url: 'https://blog.fandogh.cloud/articles/namespace-storage.html',
 		date:"۱۳ مرداد ۱۳۹۷"
-
-	},
-	{
-		title: 'چطور از دامنه شخصی برای سرویس فندق استفاده کنیم',
-		description: 'توی این آموزش خیلی سریع یک سرویس را با دامنه شخصی راه‌اندازی می‌کنیم' ,
-		image: 'https://blog.fandogh.cloud/articles/custom-domain.jpg',
-		url: 'https://blog.fandogh.cloud/articles/how-to-use-custom-domain.html',
-		date:"۱ مرداد ۱۳۹۷"
 
 	}
 ];
