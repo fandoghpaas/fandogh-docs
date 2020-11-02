3bba3e384e17d3aa2c705862cbb37d066c2e9084
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-11-02 20:42:41 +0330

    Update articles.js

--- website/articles.js
+++ website/articles.js
@@ -1,5 +1,12 @@
 const articles = [
 	{
+		title: 'داستان یک تصمیم سخت',
+		description: 'در این پست در مورد یکی از تصمیمات سخت سکوی ابری فندق با شما صحبت خواهیم کرد.',
+		image: 'https://blog.fandogh.cloud/articles/fandoghpaas-community-banner.png',
+		url:'https://blog.fandogh.cloud/articles/hard-desicion.html',
+		date:"۱۲ آبان ۱۳۹۹"
+	},
+	{
                 title: 'زیرساخت ابری به زبان ساده',
                 description: 'در این پست تلاش کردیم به صورت خلاصه زیرساخت‌های ابری رو برای عموم توضیح بدیم.',
                 image: 'https://blog.fandogh.cloud/articles/fandogh-icon.jpg',
@@ -12,14 +19,7 @@ const articles = [
                 image: 'https://blog.fandogh.cloud/articles/gitlab-ci-cd-logo.png',
                 url: 'https://blog.fandogh.cloud/articles/how-to-gitlab-ci.html',
                 date: '۱۹ بهمن ۱۳۹۷'
-        },
-	{
-		title: 'Zero Downtime Deployment',
-		description: 'در این پست به تشریح فرایند استقرار نسخه‌های جدید یک سرویس بر روی فندق می‌پردازیم. این مطالب به شما کمک خواهد کرد تا با مفهوم Zero Downtime Deployment بیشتر آشنا شوید. همچنین برای اینکه فندق بتواند این نوع استقرار را به درستی انجام دهد تنظیماتی از جانب کاربران مورد نیاز است که در این پست به تشریح آنها نیز خواهیم پرداخت.\n' ,
-		image: 'https://blog.fandogh.cloud/articles/zero-downtime.png',
-		url:'https://blog.fandogh.cloud/articles/zero-downtime.html',
-		date:"۱۸ شهریور ۱۳۹۷"
-	}
+        }
 ];
 
 module.exports = articles
